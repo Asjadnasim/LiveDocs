5ef311ab1a006d8d3bf7d166b2d94efc036c442a
Asjad Nasim <asjadnasim@gmail.com>
2024-07-22 19:43:23 +0530

    Notification documents access

--- lib/actions/room.actions.ts
+++ lib/actions/room.actions.ts
@@ -102,6 +102,21 @@ export const updateDocumentsAccess = async ({
 
 		if (room) {
 			// Todo: Send a notification
+			const notificationId = nanoid();
+
+			await liveblocks.triggerInboxNotification({
+				userId: email,
+				kind: '$documentAccess',
+				subjectId: notificationId,
+				activityData: {
+					userType,
+					title: `You have been granted ${userType} access to the document by ${updatedBy.name}`,
+					updatedBy: updatedBy.name,
+					avatar: updatedBy.avatar,
+					email: updatedBy.email,
+				},
+				roomId,
+			});
 		}
 
 		revalidatePath(`/documents/${roomId}`);
